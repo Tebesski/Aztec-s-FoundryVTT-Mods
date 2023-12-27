0fef5dd1aa4773141042dbfc2a06114c33a97097
Eli Semikashev <tebesski@gmail.com>
2023-12-27 04:16:16 +0200

    Rarity Colors 1.1.0 - Item Piles support

---
+++ Changelogs/CHANGELOG-1-1.md
@@ -0,0 +1,8 @@
+Version 1.1.0:
+
+Fixes:
+- Fixed text formatting in item sheet
+- Minor color fixes
+
+Extensions:
+- Added Item Piles support
--- rarity-colors.js
+++ rarity-colors.js
@@ -1,24 +1,24 @@
 function getItemRarityColor(rarityLevel) {
    switch (rarityLevel) {
       case "junk":
-         return ["#999", "0px 0px 1px #1F1F1F"]
+         return ["#999", "0px 0px 1px #1F1F1F", "#1F1F1F"]
       case "common":
-         return ["#00C720", "0px 0px 1px #008716"]
+         return ["#02ad1d", "0px 0px 1px #008716", "#008716"]
       case "uncommon":
-         return ["#2080ff", "0px 0px 1px #1966CC"]
+         return ["#2080ff", "0px 0px 1px #1966CC", "#1966CC"]
       case "rare":
-         return ["#FF2BF8", "0px 0px 1px #9632C7"]
+         return ["#FF2BF8", "0px 0px 1px #9632C7", "#9632C7"]
       case "legend":
-         return ["#ff8000", "0px 0px 1px #E67300"]
+         return ["#ff8000", "0px 0px 1px #E67300", "#E67300"]
       case "ancient":
-         return ["#00D980", "1px 1px 1px #00FF96"]
+         return ["#00D980", "1px 1px 1px #00FF96", "#00FF96"]
       case "dragon":
-         return ["#C2DE38", "1px 1px 1px #63611E"]
+         return ["#C2DE38", "1px 1px 1px #63611E", "#63611E"]
       case "demonic":
-         return ["#fb5ead", "1px 1px 1px #A82874"]
+         return ["#fb5ead", "1px 1px 1px #A82874", "#A82874"]
 
       default:
-         return ["#999", "0px 0px 0px #1F1F1F"]
+         return ["#999", "0px 0px 0px #1F1F1F", "#1F1F1F"]
    }
 }
 
@@ -29,9 +29,6 @@ Hooks.on("renderActorSheet", (sheet, html) => {
 
    itemsWithRarity.forEach((i) => {
       let itemID = i._id
-      console.log(itemID, "itemID")
-      let actorID = i.parent._id
-      console.log(actorID, "actorID")
       let itemRarityColor = getItemRarityColor(
          i.system.flags?.dictionary.rarity
       )
@@ -50,6 +47,61 @@ Hooks.on("renderItemSheetPF", (i, html) => {
 
       html.find(
          `input[placeholder="Item Name"]`
-      )[0].style = `color: ${itemRarityColor[0]};textShadow: ${itemRarityColor[1]}`
+      )[0].style = `color: ${itemRarityColor[0]};text-shadow: ${itemRarityColor[1]}`
    } else return
+})
+
+Hooks.on("renderItemPileInventoryApp", (pile) => {
+   const itemsWithRarity = pile.actor.allItems.filter((i) => {
+      return i.system.flags.dictionary.hasOwnProperty("rarity")
+   })
+
+   itemsWithRarity.forEach((item) => {
+      let itemRarityColor = getItemRarityColor(
+         item.system.flags?.dictionary.rarity
+      )
+
+      for (let html of pile.element.find(`.item-piles-clickable-link`)) {
+         if (html.innerText == item.name) {
+            html.style = `color: ${itemRarityColor[0]};text-shadow: ${itemRarityColor[1]};`
+         }
+      }
+   })
+})
+
+Hooks.on("renderVaultApp", (pile) => {
+   const itemsWithRarity = pile.actor.allItems.filter((i) => {
+      return i.system.flags.dictionary.hasOwnProperty("rarity")
+   })
+
+   itemsWithRarity.forEach((item) => {
+      let itemRarityColor = getItemRarityColor(
+         item.system.flags?.dictionary.rarity
+      )
+
+      for (let html of pile.element.find(`.grid-item`)) {
+         if (html.getAttribute("data-fast-tooltip") == item.name) {
+            html.style = `box-shadow: inset 0 0 10px ${itemRarityColor[0]};outline: 1px solid ${itemRarityColor[2]}`
+         }
+      }
+   })
+})
+
+Hooks.on("renderMerchantApp", (pile) => {
+   console.log(pile)
+   const itemsWithRarity = pile.merchant.allItems.filter((i) => {
+      return i.system.flags.dictionary.hasOwnProperty("rarity")
+   })
+
+   itemsWithRarity.forEach((item) => {
+      let itemRarityColor = getItemRarityColor(
+         item.system.flags?.dictionary.rarity
+      )
+
+      for (let html of pile.element.find(`.item-piles-clickable`)) {
+         if (html.innerText == item.name) {
+            html.style = `color: ${itemRarityColor[0]};text-shadow: ${itemRarityColor[1]};`
+         }
+      }
+   })
 })
